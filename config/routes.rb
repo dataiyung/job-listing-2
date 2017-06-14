c52b18831d378d220cf9d1da8eeab84c18063f5e
dataiyung <dataiyung@gmail.com>
2017-06-14 20:30:30 +1000

    implement step5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 root 'jobs#index'
+devise_for :users
 resources :jobs
 namespace :admin do
-  resources :jobs
-end
-devise_for :users
+  resources :jobs do
+  member do
+    post :publish
+    post :hide
+  end
+  end
 
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  end
